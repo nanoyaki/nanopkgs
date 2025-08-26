ea9f19bcded41a48ba1d24875a2a76aee3ef8325
nanoyaki <hanakretzer@gmail.com>
2025-08-26 06:31:17 +0200

    fix(flaresolverr): import

--- pkgs/overrides/flaresolverr.nix
+++ pkgs/overrides/flaresolverr.nix
@@ -5,5 +5,20 @@ final: prev: {
   flaresolverr = prev.flaresolverr.overrideAttrs {
     inherit (final._sources.flaresolverr) pname src;
     version = final.lib.removePrefix "v" final._sources.flaresolverr.version;
+
+    postPatch = ''
+      substituteInPlace src/undetected_chromedriver/patcher.py \
+        --replace-fail \
+          "from packaging.version import Version as LooseVersion" \
+          "from looseversion import LooseVersion"
+
+      substituteInPlace src/utils.py \
+        --replace-fail \
+          'CHROME_EXE_PATH = None' \
+          'CHROME_EXE_PATH = "${final.lib.getExe final.chromium}"' \
+        --replace-fail \
+          'PATCHED_DRIVER_PATH = None' \
+          'PATCHED_DRIVER_PATH = "${final.lib.getExe final.undetected-chromedriver}"'
+    '';
   };
 }
