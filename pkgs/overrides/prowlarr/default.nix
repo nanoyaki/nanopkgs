d1e709234295624a77bcac03ecf08c2ef3ecb743
nanoyaki <hanakretzer@nanoyaki.space>
2025-10-12 00:42:57 +0200

    fix: prowlarr..

--- pkgs/overrides/prowlarr/default.nix
+++ pkgs/overrides/prowlarr/default.nix
@@ -9,54 +9,25 @@ final: prev: {
 
       nugetDeps = builtins.map final.dotnetCorePackages.fetchNupkg (lib.importJSON ./deps.json);
     in
-    final.dotnetCorePackages.addNuGetDeps
-      {
-        nugetDeps = ./deps.json;
-        overrideFetchAttrs = old: rec {
-          runtimeIds = map (system: final.dotnetCorePackages.systemToDotnetRid system) old.meta.platforms;
-          buildInputs =
-            old.buildInputs
-            ++ lib.concatLists (lib.attrValues (lib.getAttrs runtimeIds final.dotnet-sdk_8.targetPackages));
-        };
-      }
-      {
-        inherit (final._sources.prowlarr) pname;
-        version = lib.removePrefix "v" final._sources.prowlarr.version;
 
-        src = final.applyPatches {
-          inherit (final._sources.prowlarr) src;
+    {
+      inherit (final._sources.prowlarr) pname;
+      version = lib.removePrefix "v" final._sources.prowlarr.version;
 
-          postPatch = ''
-            mv src/NuGet.config NuGet.Config
-          '';
-        };
+      src = final.applyPatches {
+        inherit (final._sources.prowlarr) src;
 
-        buildInputs =
-          nugetDeps
-          ++ (lib.concatLists (
-            lib.attrValues (
-              lib.getAttrs (map (
-                system: final.dotnetCorePackages.systemToDotnetRid system
-              ) finalAttrs.meta.platforms) final.dotnet-sdk_8.targetPackages
-            )
-          ));
+        postPatch = ''
+          mv src/NuGet.config NuGet.Config
+        '';
+      };
 
-        dotnetFlags =
-          (lib.filter (
-            flag:
-            flag != "--property:AssemblyConfiguration=master"
-            && flag != "--property:AssemblyVersion=${prevAttrs.version}"
-          ) prevAttrs.dotnetFlags)
-          ++ [
-            "--property:AssemblyConfiguration=develop"
-            "--property:AssemblyVersion=${finalAttrs.version}"
-          ];
+      buildInputs = nugetDeps ++ prevAttrs.buildInputs;
 
-        yarnOfflineCache = final.fetchYarnDeps {
-          yarnLock = "${finalAttrs.src}/yarn.lock";
-          hash = final._versions.prowlarr._yarnHash;
-        };
-      }
-      finalAttrs
+      yarnOfflineCache = final.fetchYarnDeps {
+        yarnLock = "${finalAttrs.src}/yarn.lock";
+        hash = final._versions.prowlarr._yarnHash;
+      };
+    }
   );
 }
