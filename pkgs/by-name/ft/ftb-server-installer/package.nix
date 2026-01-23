704c42e8489878212fee88c02620388e7d3f1f4e
nanoyaki <hanakretzer@nanoyaki.space>
2026-01-23 18:04:33 +0100

    fix(ftb-server-installer): remove patch, update

--- pkgs/by-name/ft/ftb-server-installer/package.nix
+++ pkgs/by-name/ft/ftb-server-installer/package.nix
@@ -10,7 +10,7 @@
 
 buildGoModule (finalAttrs: {
   pname = "ftb-server-installer";
-  version = "1.0.31";
+  version = "1.0.32";
 
   src = fetchgit {
     url = "https://github.com/FTBTeam/FTB-Server-Installer.git";
@@ -19,12 +19,10 @@ buildGoModule (finalAttrs: {
     deepClone = false;
     leaveDotGit = false;
     sparseCheckout = [ ];
-    sha256 = "sha256-PpsJAMOlOCAGY2OL0LmowIJ4WhjMqIJZKUlhoy4XQSM=";
+    sha256 = "sha256-RGiUItmqBfGZmtcPmo6xFL3T4uaP2Xewt75aVmsqiD8=";
   };
 
-  vendorHash = "sha256-j/3iys3EA9RBwqUoaN/Xjc9UmjdXrtEC+Zuk8BMSXiI=";
-
-  patches = [ ./sum.patch ];
+  vendorHash = "sha256-RN0agjtcVJSGgSAVKWhJArSmqoBQ4kQK6ac4Np1O4pU=";
 
   env.CGO_ENABLED = 0;
 
