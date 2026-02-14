e06e0a19aca7bae85a94d267a48ce2a6d7f00b9b
nanoyaki <hanakretzer@nanoyaki.space>
2026-02-14 05:51:43 +0100

    zigbee2mqtt: 2.7.2-unstable-2026-01-01 -> 2.8.0-unstable-2026-02-01
    Changelog: https://github.com/Koenkk/zigbee2mqtt/releases/tag/2.8.0-unstable-2026-02-01

--- pkgs/overrides/zigbee2mqtt/default.nix
+++ pkgs/overrides/zigbee2mqtt/default.nix
@@ -5,23 +5,23 @@ final: prev: {
   zigbee2mqtt = prev.zigbee2mqtt.overrideAttrs (
     finalAttrs: prevAttrs: {
       pname = "zigbee2mqtt";
-      version = "2.7.2-unstable-2026-01-01";
+      version = "2.8.0-unstable-2026-02-01";
 
       src = final.fetchgit {
         url = "https://github.com/Koenkk/zigbee2mqtt.git";
-        rev = "3a49c95786c2fba749e7696aab4cc38e467d2c4c";
+        rev = "b0b02c30b2e58c94043298c14848c90a0f74a24a";
         fetchSubmodules = false;
         deepClone = false;
         leaveDotGit = false;
         sparseCheckout = [ ];
-        sha256 = "sha256-eIuIWjLsjpvgIgnQC3opsSYc34GD5vLvPvO7DhKyVFA=";
+        sha256 = "sha256-O4jTVZ/9mvv6SKQ3Wrs8AcGvHoxuUjNwkHEaZkY9kBM=";
       };
 
       pnpmDeps = final.fetchPnpmDeps {
         inherit (finalAttrs) pname version src;
         pnpm = final.pnpm_9;
         fetcherVersion = 1;
-        hash = "sha256-HOYGJzcLyrvhhYnIIwhLiSiYyDdEob3+LNdlfmbspeQ=";
+        hash = "sha256-USpzlTtNlRI+xOJ37ZEohjNjtSBrq+bIqTNFH1IpObE=";
       };
 
       passthru = prevAttrs.passthru // {
